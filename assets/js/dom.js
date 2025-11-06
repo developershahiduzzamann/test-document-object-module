3e06b3f6fc11ae0a3d6ba7ce3ebe7f003d17b16e
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-11-06 22:34:37 +0600

    addad the dom file

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,2 +1,15 @@
-console.log("i fuck your system")
-console.log(document)
+const liCollection = document.getElementsByTagName('li');
+for(const li of liCollection){
+    // console.log(li.innerText);
+}
+const allHeading = document.getElementsByTagName('h1');
+for(h1 of allHeading){
+    // console.log(h1.innerText);
+}
+const allList = document.getElementsByClassName('One');
+for(const list of allList){
+    console.log(list.innerText);
+}
+
+const header = document.getElementById('title');
+ header.innerText = 'Top Three Country';
