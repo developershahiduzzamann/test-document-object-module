fc4e847e03a21a443c6fd9e7bc96a46142e893cd
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-11-14 20:31:34 +0600

    addad the element

--- assets/js/append.js
+++ assets/js/append.js
@@ -3,3 +3,12 @@ const append = document.getElementById('bookList');
 const li = document.createElement('li');
 li.innerText ='islam shikkha';
 append.appendChild(li);
+
+const mContainer = document.getElementById('m-container');
+// console.log(mContainer);
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText= 'my furuts  items';
+section.appendChild(h1);
+mContainer.appendChild(section);
