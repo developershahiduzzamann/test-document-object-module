85377a1a2b72828c45325b8af567cf0537eed8de
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-11-15 19:42:55 +0600

    addad the file change

--- assets/js/append.js
+++ assets/js/append.js
@@ -11,4 +11,20 @@ const section = document.createElement('section');
 const h1 = document.createElement('h1');
 h1.innerText= 'my furuts  items';
 section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li')
+li1.innerText = 'Apple';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li')
+li2.innerText = 'Orange';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li')
+li3.innerText = 'Banana';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
 mContainer.appendChild(section);
