498ca6fc2676c6fe80519d48e4e932ff1bfde4dc
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-11-22 01:09:15 +0600

    addad the login function

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,6 +1,12 @@
 document.getElementById('btn-update-title')
 .addEventListener('click',function (){
-    console.log('button click')
+    // console.log('button click')
     const pageTitle = document.getElementById('page-title');
     pageTitle.innerText = 'Update Page Title text';
 });
+
+document.getElementById('btn-login')
+.addEventListener('click',function(){
+    const loginPage = document.getElementById('btn-log');
+    loginPage.innerText = 'LogOut'
+})
