f92d3e069033e643f0dfc77584a2e49467699f8e
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-11-21 23:31:20 +0600

    addad the new btn

---
+++ new-btn.js
@@ -0,0 +1,6 @@
+document.getElementById('make-orange').addEventListener('click', function makeOrange(){
+    document.body.style.backgroundColor = 'orange';
+} );
+document.getElementById('make-red').addEventListener('click',function makeRed (){
+    document.body.style.backgroundColor ='red'
+});
