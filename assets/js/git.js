cc100a384bd7e8e380fbbdc7373289ee793e69d0
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-11-24 19:24:02 +0600

    addad the delete dom

---
+++ assets/js/git.js
@@ -0,0 +1,11 @@
+document.getElementById('input-delete')
+addEventListener('keyup',function(event){
+    const text = event.target.value;
+    const btnDelete = document.getElementById('btn-delete');
+    if(text === 'delete'){
+        btnDelete.removeAttribute('disabled');
+    }
+    else{
+        btnDelete.setAttribute('disabled',true);
+    }
+});
